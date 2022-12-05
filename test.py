df7cf978491804d606381220a163a5966d243c0b
MReza <killerghosts666@gmail.com>
2022-12-05 22:03:33 +0330

    3

--- test.py
+++ test.py
@@ -1,4 +1,32 @@
-import re
-txt='From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
-ext=re.findall('\S+@\S+', txt)
-print(ext)
+import urllib.request, urllib.parse, urllib.error
+import json
+TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    print('')
+    acct = input('Enter Twitter Account:')
+    if (len(acct) < 1): break
+    url = twurl.augment(TWITTER_URL,
+                        {'screen_name': acct, 'count': '5'})
+    print('Retrieving', url)
+    connection = urllib.request.urlopen(url, context=ctx)
+    data = connection.read().decode()
+
+    js = json.loads(data)
+    print(json.dumps(js, indent=2))
+
+    headers = dict(connection.getheaders())
+    print('Remaining', headers['x-rate-limit-remaining'])
+
+    for u in js['users']:
+        print(u['screen_name'])
+        if 'status' not in u:
+            print('   * No status found')
+            continue
+        s = u['status']['text']
+        print('  ', s[:50])
